15855071e68869d547eba57ec747767c018e067e
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-04-29 21:16:41 +0200

    refactor

--- src/main/java/com/tasktraker/model/Status.java
+++ src/main/java/com/tasktraker/model/Status.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
