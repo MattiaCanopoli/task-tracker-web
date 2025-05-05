925d5b26946ddfd0458f4fa6e375a30a481a6535
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-05-05 18:57:48 +0200

    chore(db) add new status "DELETED" to import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO status(status_name) VALUES ('TO-DO'),('IN-PROGRESS'),('DONE');
+INSERT INTO status(status_name) VALUES ('TO-DO'),('IN-PROGRESS'),('DONE'),('DELETED');
 INSERT INTO task (created_at,updated_at,description,user,status_id) VALUES ('2025-04-29T00:16:41.686790090','2025-04-29T00:16:41.686790090','description1','user1',1),('2025-04-29T00:16:41.686790090','2025-04-29T00:16:41.686790090','description2','user2',2);
