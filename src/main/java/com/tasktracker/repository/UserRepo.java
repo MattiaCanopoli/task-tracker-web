394299f2a73277c9c1447440f228e8f430bfae01
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-05-11 22:04:18 +0200

    refactor(user) update import for User class

--- src/main/java/com/tasktracker/repository/UserRepo.java
+++ src/main/java/com/tasktracker/repository/UserRepo.java
@@ -2,7 +2,7 @@ package com.tasktracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.tasktracker.model.User;
+import com.tasktracker.security.model.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
 
