664860fa95e486d8efc8f8e113edd9eff2c889d7
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-05-17 16:34:25 +0200

    feat(security): implement DBUserDetailsService for user authentication
    Provides a custom implementation of UserDetailsService to load users from the database.

---
+++ src/main/java/com/tasktracker/security/DBUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.tasktracker.security;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.tasktracker.repository.UserRepo;
+import com.tasktracker.security.model.User;
+
+public class DBUserDetailsService implements UserDetailsService {
+	
+	private final UserRepo userRepo;
+	
+	public DBUserDetailsService(UserRepo userRepo) {
+		this.userRepo=userRepo;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+		Optional<User> user = userRepo.findByUsername(username);
+		
+		if (user==null) {
+			throw new UsernameNotFoundException("User "+username+" not found");
+		}
+		
+		return new DBUserDetails(user.get());
+	}
+
+}
