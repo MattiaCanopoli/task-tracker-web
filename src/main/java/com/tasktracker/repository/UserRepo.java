85f1156e9c44c97fbffb4aa26aa71d38939c3d0e
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-05-17 16:26:08 +0200

    feat(repo): implemented findUserByUsername method in UserRepo
    findUserByUsername look for username in DB.
    User with corresponding username may or may not exists.
    Return an Optional<User> object that may be null.

--- src/main/java/com/tasktracker/repository/UserRepo.java
+++ src/main/java/com/tasktracker/repository/UserRepo.java
@@ -1,9 +1,13 @@
 package com.tasktracker.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tasktracker.security.model.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+	
+	public Optional<User> findByUsername(String username);
 
 }
