c2fa1715739e84fce78f4545f2b58c38dfeb7fd0
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-05-18 20:05:02 +0200

    refactor(security): move DBUserDetailsService to security.service package

--- src/main/java/com/tasktracker/security/service/DBUserDetailsService.java
+++ src/main/java/com/tasktracker/security/service/DBUserDetailsService.java
@@ -1,14 +1,17 @@
-package com.tasktracker.security;
+package com.tasktracker.security.service;
 
 import java.util.Optional;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.tasktracker.repository.UserRepo;
+import com.tasktracker.security.DBUserDetails;
 import com.tasktracker.security.model.User;
 
+@Service
 public class DBUserDetailsService implements UserDetailsService {
 	
 	private final UserRepo userRepo;
