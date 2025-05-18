adc89879e440d41fdf21adc6d57c6ad96971eb50
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-05-18 20:17:45 +0200

    chore(data): update user insert statements with BCrypt-encoded passwords

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO status(status_name) VALUES ('TO-DO'),('IN-PROGRESS'),('DONE'),('DELETED');
-INSERT INTO user (username, email,password) VALUES ('user01','user01@email.com','user01.pass'),('user02','user02@email.com','user02.pass');
+INSERT INTO user (username, email,password) VALUES ('user01','user01@email.com','$2a$12$5oTunEg83FS7Lu2H0fqkWOEXi3IRVsvT5vhHxkGC2EzNsJ3Uf3sRm'),('user02','user02@email.com','$2a$12$itrD1hxZpGbYlTdGLAcqYOYGPGGMZMSIB//nCYw/yJPbHXRDMotNC');
 INSERT INTO role (name) VALUES ('ADMIN'),('USER');
 INSERT INTO user_roles (user_id, roles_id) VALUES (1,1),(1,2),(2,2);
