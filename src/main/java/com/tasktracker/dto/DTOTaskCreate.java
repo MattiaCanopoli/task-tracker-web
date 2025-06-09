11cb39ab3f13abdc99557acb4de57b1460abe62f
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-06-09 21:51:20 +0200

    refactor(task): replace DTOTask with DTOTaskCreate and DTOTaskUpdate

--- src/main/java/com/tasktracker/dto/DTOTaskCreate.java
+++ src/main/java/com/tasktracker/dto/DTOTaskCreate.java
@@ -1,8 +1,12 @@
 package com.tasktracker.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class DTOTaskCreate {
 
-
+	@NotNull(message= "description cannot be null")
+	@NotEmpty(message= "description cannot be empty")
 	private String description;
 
 	public String getDescription() {
---
+++ src/main/java/com/tasktracker/dto/DTOTaskUpdate.java
@@ -0,0 +1,28 @@
+package com.tasktracker.dto;
+
+
+public class DTOTaskUpdate {
+
+
+	private String description;
+	private String statusName;
+
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatusName() {
+		return statusName;
+	}
+
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+
+	
+}
